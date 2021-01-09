41a3f514c255c46f5e0b1c5e7ef74a28eb3323df
rndoma <riddyndoma@gmail.com>
2021-01-09 03:08:37 +0200

    verification and addition of missing important columns

---
+++ functions/important_columns.R
@@ -0,0 +1,24 @@
+######################################################################
+followups_base_columns <- c(
+  'questionnaireAnswers.lait_matenel',
+  'questionnaireAnswers.genex_date',
+  'questionnaireAnswers.gen_gp',
+  'questionnaireAnswers.gen_np',
+  'questionnaireAnswers.exa_biochimie',
+  'questionnaireAnswers.hematologie',
+  'questionnaireAnswers.urine',
+  'questionnaireAnswers.sperme',
+  'questionnaireAnswers.gene_date',
+  'questionnaireAnswers.gene_resultat',
+  'questionnaireAnswers.cmd_np',
+  'questionnaireAnswers.var5_est_ce_un_probleme_que_vous_avez_eu_avant_ebola_sinon_completer_la_colonne_suivante'
+)
+
+######################################################################
+#Add missed columns in the tibble
+AddingColumns <- function(data, cname) {
+  add <-cname[!cname%in%names(data)]
+  
+  if(length(add)!=0) data[add] <- NA
+  data
+}
