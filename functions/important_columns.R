7837278b9bd3c2cbae9fac9f44ed2aca22b08529
rndoma <riddyndoma@gmail.com>
2021-01-12 12:37:48 +0200

    Precision of importants columns of followers Tibble

--- functions/important_columns.R
+++ functions/important_columns.R
@@ -11,7 +11,14 @@ followups_base_columns <- c(
   'questionnaireAnswers.gene_date',
   'questionnaireAnswers.gene_resultat',
   'questionnaireAnswers.cmd_np',
-  'questionnaireAnswers.var5_est_ce_un_probleme_que_vous_avez_eu_avant_ebola_sinon_completer_la_colonne_suivante'
+  'questionnaireAnswers.var5_est_ce_un_probleme_que_vous_avez_eu_avant_ebola_sinon_completer_la_colonne_suivante',
+  'questionnaireAnswers.test_vih',
+  'questionnaireAnswers.tdr_palu',
+  'questionnaireAnswers.grossesse',
+  'questionnaireAnswers.lait_matenel_value',
+  'questionnaireAnswers.genex_date_value',
+  'questionnaireAnswers.gen_gp_value',
+  'questionnaireAnswers.gen_np_value'
 )
 
 ######################################################################
